e95e3d95361322e9d3311945c94538a0301d5e8c
Peter Leander <pleander@student.hive.fi>
2024-07-17 13:49:50 +0300

    Fix norm

--- utils.c
+++ utils.c
@@ -46,7 +46,7 @@ int	str_is_valid(char *str)
 			return (0);
 		if (str[i] == '-'
 			&& ((i != 0 && !ft_isspace(str[i - 1]))
-			|| !ft_isdigit(str[i + 1])))
+				|| !ft_isdigit(str[i + 1])))
 			return (0);
 		i++;
 	}
